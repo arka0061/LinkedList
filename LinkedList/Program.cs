94f78dab1d6252cf7acca0c96718d8d0120b221f
Arka <arka.spectacular@gmail.com>
2021-08-06 17:57:18 +0530

    [Arka]Added:Insert Element in middle

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -9,15 +9,28 @@ namespace LinkedList
             Console.WriteLine("Welcome to Linked List Assignment!");
             LinkedLists list = new LinkedLists();
             list.Add(56);
+            list.Add(30);
             list.Add(70);
-            list.InsertAtParticularPosition(1, 30);
-            list.PopLastElement();
-            list.Display();         
             int check = list.Search(30);
-            if(check==1)
+            if (check == 1)
             {
-                Console.WriteLine("Element is Present");
+                Console.WriteLine("Node is Present!");
+                list.insertAtMid(40);
+                list.Display();
             }
+            else
+            {
+                Console.WriteLine("Node is absent!");
+            }
+
+
+
+
+
+            // list.InsertAtParticularPosition(1, 30);
+            // list.PopLastElement();
+                   
+
         }
     }
 }
